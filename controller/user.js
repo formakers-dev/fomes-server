0b51165cb63c6432aa508ea6a96a1e5eb3421cd5
Jason Ryu and Steve Jeong <pair+sryu99+optyyie@gmail.com>
2017-08-02 16:22:10 +0900

    upsertUser 에러코드 변경 403 -> 500 googleTokenVerifier 불필요한 로직 삭제

--- controller/user.js
+++ controller/user.js
@@ -2,41 +2,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const config = require('../config');
 
-let generateToken = (req, res) => {
-    const generateToken = (user) => {
-        return new Promise(
-            (resolve, reject) => {
-                jwt.sign(user, config.secret, {
-                    expiresIn: '7d',
-                    issuer: 'appbee.com',
-                    subject: 'AppBeeAuth'
-                }, (err, newToken) => {
-                    if(err) {
-                        reject(err);
-                    } else {
-                        resolve(newToken);
-                    }
-                });
-            }
-        );
-    };
-
-    generateToken(req.user)
-        .then((token) => {
-            console.log(token);
-            res.json(token);
-        })
-        .catch((err) => {
+const generateToken = (req, res) => {
+    jwt.sign(req.user, config.secret, {
+        expiresIn: '7d',
+        issuer: 'appbee.com',
+        subject: 'AppBeeAuth'
+    }, (err, newToken) => {
+        if(err) {
             console.log("====generateToken:Error" + err.message);
             res.status(403).json({
                 success: false,
                 message: err.message
             });
-        });
+        } else {
+            res.json(newToken);
+        }
+    });
 };
 
-
-let upsertUser = (req, res, next) => {
+const upsertUser = (req, res, next) => {
     User.findOneAndUpdate({userId : req.user.userId}, { $set: req.user }, {upsert: true})
         .exec()
         .then(() => {
@@ -44,7 +28,7 @@ let upsertUser = (req, res, next) => {
         })
         .catch((err) => {
             console.log('===upsertUser:Error' + err.message);
-            res.status(403).json({
+            res.status(500).json({
                 success: false,
                 message: err.message
             });
--- middleware/googleTokenVerifier.js
+++ middleware/googleTokenVerifier.js
@@ -1,19 +1,15 @@
 const GoogleAuth = require('google-auth-library');
 
 let googleTokenVerifier = (req, res, next) => {
-    const googleIdToken = req.headers['x-id-token'];
-
-    const verifyGoogleToken = () => {
+    const verifyGoogleToken = (googleIdToken) => {
         return new Promise(
             (resolve, reject) => {
-                const CLIENT_ID = process.env['GG_CLIENT_ID'];
-                const SECRET_KEY = process.env['GG_SECRET_KEY'];
                 const auth = new GoogleAuth;
-                const client = new auth.OAuth2(CLIENT_ID, SECRET_KEY);
+                const client = new auth.OAuth2();
 
                 client.verifyIdToken(
                     googleIdToken,
-                    CLIENT_ID,
+                    process.env['GG_CLIENT_ID'],
                     function(err, login) {
                         if (err) {
                             reject(err);
@@ -25,22 +21,7 @@ let googleTokenVerifier = (req, res, next) => {
                             user.email = payload['email'];
                             user.name = payload['name'];
                             user.provider = 'GG';
-                            /*
-                               {  azp: '1011092046994-deh2fceumbfo9quv2qcsf0cp36nou6bj.apps.googleusercontent.com',
-                                  aud: '1011092046994-qdvlddoem775qt59sm5b2sqfp3iq0etf.apps.googleusercontent.com',
-                                  sub: '110897406327517511196',
-                                  email: 'yenarue@gmail.com',
-                                  email_verified: true,
-                                  iss: 'https://accounts.google.com',
-                                  iat: 1501539255,
-                                  exp: 1501542855,
-                                  name: '김예나',
-                                  picture: 'https://lh5.googleusercontent.com/-F60UiYfCWWo/AAAAAAAAAAI/AAAAAAAAAAA/AMp5VUoa5-bAyIk_qWlnGVcG9SO-GucAXw/s96-c/photo.jpg',
-                                  given_name: '예나',
-                                  family_name: '김',
-                                  locale: 'ko'
-                                }
-                            */
+
                             resolve(user);
                         }
                     }
@@ -49,24 +30,18 @@ let googleTokenVerifier = (req, res, next) => {
         );
     };
 
-    const onError = (err) => {
-        console.log('===verifyGoogleToken:onError');
-        res.status(403).json({
-            success: false,
-            message: err.message
+    verifyGoogleToken(req.headers['x-id-token'])
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch((err) => {
+            console.log('===verifyGoogleToken:onError');
+            res.status(403).json({
+                success: false,
+                message: err.message
+            });
         });
-    };
-
-    const onSuccess = (user) => {
-        console.log('===verifyGoogleToken:onSuccess');
-        req.user = user;
-        next();
-    };
-
-    verifyGoogleToken()
-        .then(onSuccess)
-        .catch(onError);
 };
-
 
 module.exports = googleTokenVerifier;
