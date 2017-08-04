ff389357785fba2d8c1cf56cad3fdcacd6f65c3c
Jason Ryu and Yena Kim <pair+sryu99+yenarue@gmail.com>
2017-08-04 15:25:57 +0900

    단기통계 id기준 data 축적

--- controller/shortTermStats.js
+++ controller/shortTermStats.js
@@ -1,18 +1,14 @@
 let ShortTermStats = require('./../models/shortTermStats');
 
 let postShortTermStats = (req, res) => {
-    let shortTermStatJson = {};
-    shortTermStatJson.userId = req.userId;
-    shortTermStatJson.stats = req.body;
-
-    let newShortTermStats = new ShortTermStats(shortTermStatJson);
-    newShortTermStats.save((err) => {
-        if(err) {
-            res.send(err);
-        }else {
+    ShortTermStats.findOneAndUpdate({userId : req.userId}, {$push: {'stats' : req.body}}, {upsert: true})
+        .exec()
+        .then(() => {
             res.send(true);
-        }
-    })
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 };
 
 module.exports = {postShortTermStats};
