dbbcb5fa499c61ba3659fb462ea233c51b042417
Jason Ryu and Steve Jeong <pair+sryu99+optyyie@gmail.com>
2017-08-08 18:58:32 +0900

    단기앱정보API 데이터 테스트 추가

--- test/shortTermStatsTest.js
+++ test/shortTermStatsTest.js
@@ -29,7 +29,14 @@ describe('shortTermStats', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.eql(true);
-                    done();
+
+                    ShortTermStats.findOne({userId : config.testUserId}, (err, shortTermStat) => {
+                        shortTermStat.stats.length.should.be.eql(2);
+                        verifyShortTermStatData(shortTermStat.stats[0], 'com.whatever.package1', 1499914700000, 1499914800000, 100000);
+                        verifyShortTermStatData(shortTermStat.stats[1], 'com.whatever.package2', 1499914700001, 1499914900001, 200000);
+
+                        done();
+                    });
                 });
         });
 
@@ -40,7 +47,8 @@ describe('shortTermStats', () => {
                 .send(doc)
                 .end((err, res) => {
                     res.should.have.status(403);
-                    done();
+
+                    verifyDoNotInsertShortTermStatData(done);
                 });
         });
 
@@ -51,9 +59,25 @@ describe('shortTermStats', () => {
                 .send(doc)
                 .end((err, res) => {
                     res.should.have.status(401);
-                    done();
+
+                    verifyDoNotInsertShortTermStatData(done);
                 });
         });
+
+        const verifyShortTermStatData = function(shortTermStat, packageName, startTimeStamp, endTimeStamp, totalUsedTime) {
+            shortTermStat.packageName.should.be.eql(packageName);
+            shortTermStat.startTimeStamp.should.be.eql(startTimeStamp);
+            shortTermStat.endTimeStamp.should.be.eql(endTimeStamp);
+            shortTermStat.totalUsedTime.should.be.eql(totalUsedTime);
+        };
+
+        const verifyDoNotInsertShortTermStatData = function(done) {
+            ShortTermStats.findOne({userId : config.testUserId}, (err, shortTermStat) => {
+                chai.expect(shortTermStat).to.be.null;
+
+                done();
+            });
+        };
     });
 
     afterEach((done) => {
