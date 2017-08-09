64a4c01bf51e53e3ca05fbeca10e767d52c88b83
Steve Jeong and Tiffany Kim <pair+optyyie+yoonyoungkim89@gmail.com>
2017-08-09 17:07:29 +0900

    사용자 정보 API 데이터 구조 테스트 추가

--- test/userTest.js
+++ test/userTest.js
@@ -3,7 +3,6 @@ const chaiHttp = require('chai-http');
 const config = require('../config')[process.env.NODE_ENV];
 const should = chai.should();
 const Users = require('../models/user');
-const expect = chai.expect;
 const jwt = require('jsonwebtoken');
 require('./setupSinon')();
 const server = require('../server');
@@ -12,6 +11,18 @@ chai.use(chaiHttp);
 
 describe('Users', () => {
     describe('GET user auth', () => {
+        const newUser = {
+            userId: 'testUserId',
+            provider: 'google',
+            name: '테스트유저',
+            email: 'appbee0627@gmail.com'
+        };
+        const oldUser = {
+            userId: 'testUserId',
+            provider: 'facebook',
+            name: '이름변경된테스트유저',
+            email: 'appbee1231@gmail.com'
+        };
         it('google id토큰 검증 후 API 사용을 위한 appbee 토큰을 발급하여 리턴한다', done => {
             chai.request(server)
                 .get('/user/auth')
@@ -32,6 +43,40 @@ describe('Users', () => {
                     });
                 });
         });
+
+        it('새로운 사용자일 경우, 유저정보를 정상적으로 저장한다', done => {
+            Users.findOneAndUpdate({userId: newUser.userId}, {$set: newUser}, {upsert: true})
+                .exec()
+                .then(() => {
+                    Users.findOne({userId: newUser.userId}, (err, user) => {
+                        verifyUserData(user, "testUserId", "google", "테스트유저", "appbee0627@gmail.com");
+                        done();
+                    });
+
+                });
+        });
+
+        it('기존 사용자일 경우, 유저정보를 정상적으로 업데이트한다', done => {
+            Users.findOneAndUpdate({userId: newUser.userId}, {$set: newUser}, {upsert: true})
+                .exec()
+                .then(() => {
+                    Users.findOneAndUpdate({userId: oldUser.userId}, {$set: oldUser}, {upsert: true})
+                        .exec()
+                        .then(() => {
+                            Users.findOne({userId: oldUser.userId}, (err, user) => {
+                                verifyUserData(user, "testUserId", "facebook", "이름변경된테스트유저", "appbee1231@gmail.com");
+                                done();
+                            });
+                        });
+                });
+        });
+
+        const verifyUserData = (user, userId, provider, name, email) => {
+            user.userId.should.be.eql(userId);
+            user.provider.should.be.eql(provider);
+            user.name.should.be.eql(name);
+            user.email.should.be.eql(email);
+        };
 
         afterEach((done) => {
             Users.remove({ userId : config.testUserId }, () => {
