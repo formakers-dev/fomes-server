5dc46f7932724fcd87af430916f344f97db6277c
Jason Ryu and Yena Kim <pair+sryu99+yenarue@gmail.com>
2017-09-18 19:01:43 +0900

    미사용 EventStat API 삭제

--- router/stats.js
+++ router/stats.js
@@ -2,18 +2,15 @@ const express = require('express');
 const statsRouter = express.Router();
 const ShortTermStats = require('../controller/shortTermStats');
 const LongTermStats = require('../controller/longTermStats');
-const EventStats = require('../controller/eventStats');
 const Analyses = require('../controller/analyses');
 const Auth = require('../middleware/auth');
 
 statsRouter.route('/short')
     .post(Auth.appBeeTokenVerifier, ShortTermStats.postShortTermStats);
 
-statsRouter.route('/event')
-    .post(Auth.appBeeTokenVerifier, EventStats.postEventStats);
-
 statsRouter.route('/long/yearly')
     .post(Auth.appBeeTokenVerifier, LongTermStats.postLongTermStatsBy2years);
+
 statsRouter.route('/long/monthly')
     .post(Auth.appBeeTokenVerifier, LongTermStats.postLongTermStatsBy3months);
 
