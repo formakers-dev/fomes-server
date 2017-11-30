3cff0338f7df50cb048394df254210f373ce75a9
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-11-30 19:48:02 +0900

    최소앱버전코드조회 API 반환형식 변경 : Json -> number

--- controller/configurations.js
+++ controller/configurations.js
@@ -3,9 +3,7 @@ const Configurations = require('./../models/configurations');
 const getMinAppVersionCode = (req, res) => {
     Configurations.findOne({}).select('minAppVersionCode')
         .exec()
-        .then(configuration => res.send({
-            version: configuration.minAppVersionCode
-        }))
+        .then(configuration => res.json(configuration.minAppVersionCode))
         .catch(err => {
             console.log(err);
             res.send(err);
