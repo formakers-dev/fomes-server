85fa51fcca6c4ada05d28000b41d8dc2faac9313
Jason Ryu <sryu99@gmail.com>
2018-11-03 08:23:23 +0900

    refactor: Recommend Controller 로직 분리
    - Recommend Service 생성 : 내부적으로 App, AppUsage, User Service 사용
    - Paging Util 생성 : 페이징 관련 검증로직
    
    https://trello.com/c/ommJGQf4

---
+++ services/recommendApps.js
@@ -0,0 +1,75 @@
+const UserService = require('../services/users');
+const AppUsageService = require('../services/appUsages');
+const AppService = require('../services/apps');
+
+const RecommendType = {
+    favoriteGame: 1,
+    favoriteDeveloper: 2,
+    favoriteCategory: 3,
+    similarDemographic: 4,
+};
+
+const getSimilarUserRecommendApps = (userId, page, limit) => {
+    const recommendInfo = {recommendType: RecommendType.similarDemographic, criteria: []};
+
+    return UserService.getUser(userId)
+        .then(user => {
+            recommendInfo.criteria.push(UserService.getAge(user.birthday) + "대");
+            recommendInfo.criteria.push(user.gender === "male" ? "남성" : "여성");
+            return AppUsageService.getSimilarUsers(user, parseInt(page), parseInt(limit));
+        })
+        .then(appUsages => convertToRecommendApps(recommendInfo, appUsages, userId));
+};
+
+const getFavoriteCategoryRecommendApps = (categoryUsages, userId) => {
+    if (!categoryUsages || categoryUsages.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    const recommendInfo = {recommendType: RecommendType.favoriteCategory, criteria: []};
+    const sortedCategoryUsages = categoryUsages.sort((a, b) => a.totalUsedTime > b.totalUsedTime ? -1 : 1);
+
+    return Promise.resolve(sortedCategoryUsages[0])
+        .then(favoriteCategoryUsage => {
+            recommendInfo.criteria.push(favoriteCategoryUsage.name);
+            return AppUsageService.getCategoryAppUsages(favoriteCategoryUsage.id);
+        })
+        .then(appUsages => convertToRecommendApps(recommendInfo, appUsages, userId));
+};
+
+const getFavoriteDeveloperRecommendApps = (developerUsages, userId) => {
+    if (!developerUsages || developerUsages.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    const recommendInfo = {recommendType: RecommendType.favoriteDeveloper, criteria: []};
+    const sortedDeveloperUsages = developerUsages.sort((a, b) => a.totalUsedTime > b.totalUsedTime ? -1 : 1);
+
+    return Promise.resolve(sortedDeveloperUsages[0])
+        .then(favoriteDeveloperUsage => {
+            recommendInfo.criteria.push(favoriteDeveloperUsage.name);
+            return AppUsageService.getDeveloperAppUsages(favoriteDeveloperUsage.id);
+        })
+        .then(appUsages => convertToRecommendApps(recommendInfo, appUsages, userId));
+};
+
+const convertToRecommendApps = (recommendInfo, appUsages, userId) => {
+    return Promise.resolve(appUsages.filter(i => i.developer && i.categoryId))
+        .then(appUsages => AppService.combineAppInfos(appUsages))
+        .then(appUsagesWithAppInfo => Promise.resolve(
+            appUsagesWithAppInfo.map(item => {
+                return {
+                    recommendType: recommendInfo.recommendType,
+                    criteria: recommendInfo.criteria,
+                    app: replaceWishedByToWishedByMe(item, userId)
+                };
+            })));
+};
+
+const replaceWishedByToWishedByMe = (item, userId) => {
+    item.wishedByMe = !!(item.wishedBy && item.wishedBy.includes(userId));
+    delete item.wishedBy;
+    return item;
+};
+
+module.exports = {getSimilarUserRecommendApps, getFavoriteCategoryRecommendApps, getFavoriteDeveloperRecommendApps};
