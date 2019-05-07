c12ab40f6ab4c351620b68f74519dd1e7c69ba4e
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2019-05-07 19:12:47 +0900

    feat: Firebase Analytics 로깅을 위해 Post 스키마에 ObjectId 필드 추가
    https://trello.com/c/1hQ77xGv

--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     order: Number,              // 순서 정렬용
     openDate: Date,             // 게시 시작일시
     closeDate : Date,           // 게시 종료일시
