34c829a2559345219fb9d1f1550a4d6b5c8869ad
Jingi Kim <copyx00@gmail.com>
2019-07-29 01:33:04 +0000

    Merged in gracefule_exit_for_db (pull request #5)
    feat: 서버 프로세스 종료 시 DB 연결 해제 코드 추가
    
    그 외
    - connect 시 useNewUrlParser 옵션 적용
    
    https://trello.com/c/FVlxoLrs
    
    Approved-by: Yena Kim <yenarue@gmail.com>

--- db.js
+++ db.js
@@ -1,23 +1,44 @@
 const mongoose = require('mongoose');
 const config = require('./config');
 
-const connect = () => {
-    mongoose.connect(config.dbUrl, function(err) {
-        if (err) {
-            console.error('mongodb connection error', err);
-        } else {
-            console.log('mongodb connected');
-        }
-    });
-};
-
-const setRecoverConfig = () => {
-    mongoose.connection.on('disconnected', connect);
-};
+const TAG = '[DB]';
 
 const init = () => {
-    connect();
-    setRecoverConfig();
+    const gracefulExit = cause => {
+        mongoose.disconnect()
+            .then( () => {
+                console.log(TAG, `All connections are disconnected due to exit process by ${cause}`);
+                process.exit();
+            })
+            .catch(err => {
+                console.error(TAG, `Error occured on disconnecting due to exit process by ${cause}:\n`, err);
+                process.exit();
+            });
+    };
+
+    ['exit', 'SIGINT', 'SIGTERM', 'SIGQUIT'].forEach(signal => {
+        process.on(signal, gracefulExit);
+    });
+
+    process.on('uncaughtException', err => {
+        console.error('Uncaught Exception:', err);
+        gracefulExit('uncaught exception');
+    });
+
+    mongoose.connection.on('connected', () => {
+        console.log(TAG, 'Default connection is connected');
+    });
+    mongoose.connection.on('error', err => {
+        console.error(TAG, 'Default connection has occurred error:', err);
+    });
+    mongoose.connection.on('disconnected', () => {
+        console.log(TAG, 'Default connection is disconnected');
+    });
+
+    mongoose.connect(config.dbUrl, {useNewUrlParser: true})
+        .catch(err => {
+            console.error(TAG, 'Error is occurred on connecting:', err);
+        });
 };
 
 module.exports = {init};
