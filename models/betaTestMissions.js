c0ef31ed1a461e43c4163e68375ce45f9a990908
yenarue <yenarue@gmail.com>
2020-04-09 19:04:22 +0900

    fixed: betaTestMissions 모델 오입력된 필드명 재변경 (deeplink)

--- models/betaTestMissions.js
+++ models/betaTestMissions.js
@@ -8,7 +8,7 @@ const schema = new Schema({
     description: String,
     descriptionImageUrl: String,
     guide: String,
-    deepLink: String,
+    deeplink: String,
     options: Array,
     type: String,
 
